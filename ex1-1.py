046bbd0a4800eee499dba2fe650066824aa567cf
Sara Kilany <44653974+sarakilany@users.noreply.github.com>
2020-11-01 23:03:04 +0200

    Think Python Ex1-1 solution

---
+++ ex1-1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 00:32:32 2020
+
+@author: Ouso
+"""
+#ex1-1
+print('Ex1-1 \n')
+'''1.In a print statement, what happens if you leave out one of the 
+      'parentheses, or both?'''
+print("Ans 1. Gives syntax error SyntaxError: Missing parentheses in call to "
+      " 'print'. Did you mean print('I am Sara')? \n")
+
+'''2. If you are trying to print a string, what happens if you leave"
+      "out one of the quota‐tion marks, or both?'''
+print("Ans 2. It gives syntax error SyntaxError: EOL while scanning"
+      "string literal \n" )
+
+'''3. You can use a minus sign to make a negative number like -2."
+      "What happens if you put a plus sign before a number? What about 2++2?'''
+print("Ans 3. It adds the two numbers normally, the answer is 4 \n ")
+
+'''4. In math notation, leading zeros are okay, as in 02. What happens"
+      "if you try this in Python?'''
+print("Ans 4. It gives Syntax error : SyntaxError: invalid token \n ")
+
+'''5. What happens if you have two values with no operator between them? '''
+print("Ans 5. it gives syntax error SyntaxError: invalid syntax \n ")
+
+print('\n')
+
