1015c7f7b1d648e2991acba62c7c088073b793d0
Hadeel Salah <42890131+hadeelsala7@users.noreply.github.com>
2020-11-07 15:14:04 +0200

    Create Chapter2-Ex1.py

---
+++ Chapter2-Ex1.py
@@ -0,0 +1,12 @@
+print("We have seen that n = 42 is legal. What about 42 = n? \n it's not legal too")
+print('-------------------------------------------------')
+
+print('How about x = y = 1? \n its legal' )
+print('-------------------------------------------------')
+print('• In some languages every statement ends with a semi-colon, ;. What happens if you put a semi-colon at the end of a Python statement? \n it will work as normal');
+print('-------------------------------------------------')
+
+print('What if you put a period at the end of a statement? \n sytanx error')
+print('-------------------------------------------------')
+
+print('In math notation you can multiply x and y like this: xy. What happens if you try that in Python? \n xy is not defined')
