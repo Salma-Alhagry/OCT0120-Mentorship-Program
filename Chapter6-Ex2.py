3346a668f92eab0ab25772232039cd57eb2eefeb
Hadeel Salah <42890131+hadeelsala7@users.noreply.github.com>
2020-11-07 15:56:42 +0200

    Create Chapter6-Ex2.py

---
+++ Chapter6-Ex2.py
@@ -0,0 +1,12 @@
+def ack(m, n):
+    """Ackermann's function;
+    m, n - integers greater-than-equal 0
+    """
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ack(m - 1, 1)
+    elif m > 0 and n > 0:
+        return ack(m - 1, ack(m, n - 1))
+
+print(ack(3, 4))
